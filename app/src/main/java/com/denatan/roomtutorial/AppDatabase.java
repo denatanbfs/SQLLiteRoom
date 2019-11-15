19d31f113612eff88328c4f955f5ac81250f1bf5
DenatanBFS <denatanbagusfs@gmail.com>
2019-11-15 14:17:44 +0700

    init commit

---
+++ app/src/main/java/com/denatan/roomtutorial/AppDatabase.java
@@ -0,0 +1,10 @@
+package com.denatan.roomtutorial;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Barang.class}, version = 1)
+
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract BarangDAO barangDAO();
+}
---
+++ app/src/main/java/com/denatan/roomtutorial/BarangDAO.java
@@ -0,0 +1,7 @@
+package com.denatan.roomtutorial;
+
+import androidx.room.Dao;
+
+@Dao
+public interface BarangDAO {
+}
